6c2b95fb0985ec91cb1e8af31471a746345a9013
Joe Douglas <joe.douglas@ardoq.com>
2022-04-04 13:04:29 +0100

    Fiddlin

--- projects/conclave/dev/fiddle.cljs
+++ projects/conclave/dev/fiddle.cljs
@@ -1,19 +1,64 @@
 (ns conclave.fiddle
-  (:require [conclave.map.core :as map]
-            [conclave.map.layout :as layout]
+  (:require [conclave.map.beta.build :as map.build]
+            [conclave.map.beta.constraint :as constraint]
+            [conclave.map.beta.distance :as distance]
+            [conclave.map.beta.optimization :as optimization]
+            [conclave.map.beta.score :as score]
+            [conclave.map.beta.stake :as stake]
+            [conclave.map.beta.summary :as summary]
             [conclave.hex :as hex]
-            [clojure.math.combinatorics :as combo]))
+            [conclave.map.core :as core]
+            [conclave.map.layout :as layout]
+            [conclave.score :as util-score]
+            [conclave.tiles.core :as tile]
+            [clojure.math.combinatorics :as combo]
+            [clojure.spec.alpha :as s]
+            [medley.core :as medley]
+            [taoensso.tufte :as tufte :refer-macros (profiled)]))
 
+(def sample-map (map.build/create "ABCDE"))
+(def swaps (core/generate-swap-list sample-map "ABCDE"))
+(first swaps)
+
+(s/valid? (s/map-of ::layout/coordinate
+                    (s/map-of keyword?
+                              (s/map-of ::layout/coordinate
+                                        number?)))
+          (score/shares-for-map sample-map))
+
+(->> (score/combined-shares sample-map)
+     (score/invert-share-map)
+     (medley/map-vals #(->> %
+                            score/apply-weights
+                            vals
+                            (apply +))))
 (comment
-  (-> (layout/free-spaces layout/eight-player)
-      (combo/combinations 2)
-      count)
+  "Demonstrating that you don't need to consider variation for constrained tiles
+   (assuming that the min-distance is 3)"
+  (->> (combo/selections [1 2 3 4 5] 3)
+       (filter (comp (partial = 3)
+                     (partial apply min)))
+       (map (juxt identity util-score/variation))
+       (sort-by second)))
 
-  (let [rings (map hex/ring-coordinates (range 1 5))
-        combos (combo/combinations rings 2)
-        swaps (combo/combinations (apply concat rings) 2)
-        inter-ring (mapcat (partial apply combo/cartesian-product) combos)
-        intra-ring (mapcat #(combo/combinations % 2) rings)]
-    [(count swaps)
-     (count inter-ring)
-     (count intra-ring)]))
+(constraint/evaluate-constraint sample-map (first constraint/constraints))
+(constraint/evaluate-constraints sample-map)
+(constraint/score sample-map)
+
+(score/combined-shares sample-map)
+
+(core/coordinate->tile sample-map [-1 1 0])
+(-> (apply core/swap-tiles sample-map (first swaps))
+    (core/coordinate->tile [0 -1 1]))
+
+(drop 1 (optimization/to-triple sample-map))
+(drop 1 (optimization/to-triple (apply core/swap-tiles sample-map (second swaps))))
+(let [[m v c] (optimization/to-triple sample-map)]
+  (drop 1 (optimization/step m v c (second swaps))))
+
+(-> (profiled {} (optimization/optimize sample-map swaps))
+    second
+    (tufte/format-pstats {:columns [:n-calls :p50 :mean :clock :total]})
+    println)
+
+(summary/player-summary sample-map :p1)
